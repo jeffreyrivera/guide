78dd2906b4085b98923028b41ec6f53291dd60e6
Jeffrey Rivera <jeffreyrivera@gmail.com>
2017-10-09 23:02:33 -0400

    initial commit git guide in

---
+++ git_guide.py
@@ -0,0 +1,18 @@
+GIT SET UP
+
+# Initialize git config
+git config --global user.name "John Doe"
+git config --global user.email johndoe@example.com
+git config --list
+
+#intialize new
+git init 
+
+#clone
+git clone https://github.com/schacon/simplegit-progit
+
+# https://git-scm.com/book/en/v2/Git-Basics-Recording-Changes-to-the-Repository
+
+# story
+git log
+
